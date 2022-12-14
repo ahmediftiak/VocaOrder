f9ecaf5503bf73711fdb2d9a5c9541fdd7a69fc5
Iftiak Ahmed <ahmediftiak1199@gmail.com>
2022-12-14 14:01:55 -0500

    CSS changes

--- client/src/pages/AboutUsPage.js
+++ client/src/pages/AboutUsPage.js
@@ -4,34 +4,38 @@ function AboutUsPage(props) {
   return (
     <>
       <div className="col text-center">
+        <div className="about">
         <h2 className="mb-3">About our project</h2>
         <p className="mb-5">
           VocaOrder is web-based food ordering system through use of voice coversation.
         </p>
-        <h2 className="mb-3">About our Team</h2>
+        </div>
+        <div className="aboutteam">
+        <h2 className="mb-5">About our Team</h2>
         <div className="row">
-          <div className="col-lg-4">
+          <div className="person col-lg-4">
             <h3>Choeden Dolma</h3>
             <p>
               A computer science student at Groove School of engineering 
               in City College of New York and fellow at Cuny Tech-Prep.
             </p>
           </div>
-          <div className="col-lg-4">
+          <div className="person col-lg-4">
             <h3>Shahid Emdad</h3>
             <p>
             A computer science student at Groove School of engineering 
             in City College of New York and fellow at Cuny Tech-Prep.
             </p>
           </div>
-          <div className="col-lg-4">
+          <div className="person col-lg-4">
             <h3>Iftiak Ahmed</h3>
             <p>
-            A computer science student at Groove School of engineering 
+            A computer engineering student at Groove School of engineering 
             in City College of New York and fellow at Cuny Tech-Prep.
             </p>
           </div>
         </div>
+      </div>
       </div>
     </>
   );
